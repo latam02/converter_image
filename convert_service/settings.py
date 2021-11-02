b786e8af2d306368dbe6f83d1aa2d2dfcaaee35c
christiantc <christiantambo15@gmail.com>
2021-11-02 19:16:49 -0400

    task7

--- convert_service/settings.py
+++ convert_service/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure--#nd()e$g4j9+p)l7gfjh+&p9m_irwokkm8yz0ofkm86o!z!&d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.33.60']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
